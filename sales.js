43aa0a683d3386b20eafb9f37fe7fae26ad75080
Jude <jnguyen41@mail.csuchico.edu>
2016-10-31 17:35:42 -0700

    addItem() function updated

--- sales.js
+++ sales.js
@@ -2,23 +2,20 @@ var runningTotal = 0.0;
 
 function addItem()
 {
+  var dollars;
   var newItem;
   newItem = document.getElementById("price").value;
   var check = isNaN(newItem); //returns false
   if(check){
-    alert("Please enter a valid number.");
+    alert("Enter price as a number.");
   }
   else{
-    alert("That is correct.");
+    newItem = Number(newItem);
+    runningTotal=runningTotal+newItem;
+    dollars = asCurrency(runningTotal);
+    document.getElementById("price").value="";
+    setCookie(preTax, runningTotal);
   }
-  //IF newItem is not a number
-  // THEN show an alert: "Enter price as a number"
-  //OTHERWISE,
-  // update newItem to its value cast as a number
-  // update runningTotal to be its value plus newItem
-  // create a variable called dollars
-  // call asCurrency() by with the value of runningTotal and assign the return value to dollars
-  // update the innerHTML of the span with the id "subtotal" to be dollars
   // update the value of the input with the id "price" to be an empty string
   // update a cookie called "preTax" with the value of runningTotal
 }
